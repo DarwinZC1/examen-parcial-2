e7e393ee088d34c662632fcc2f7dcfc5f3990249
DarwinZC1 <132626111+DarwinZC1@users.noreply.github.com>
2023-05-24 22:04:00 -0400

    Add files via upload

---
+++ examen parcial 2/script.js
@@ -0,0 +1,67 @@
+let uno = true;
+let dos = "2";
+let tres = 3;
+let cuatro = null;
+let cinco;
+let seis = {numero: 6};
+let siete = [7];
+
+console.log(typeof'true');
+console.log(typeof 2);
+console.log(typeof 3);
+console.log(typeof null);
+console.log(typeof notDefined);
+console.log(typeof {numero: 6});
+console.log(typeof [7]);
+
+
+let nombreProducto = "coca cola" , precio = "10bs", direccionEnvio = "Santa Cruz", país = "Bolivia", costoEnvio = "200bs"
+
+let Libreria = "React es una biblioteca Javascript de código abierto creada por Facebook"
+console.log(Libreria.length);
+
+console.log(Libreria.replace("Javascript de código abierto creada por Facebook","por ReactNative es un framework"));
+
+
+
+let autos = "TOYOTA, SUZUKI, BMW, FORD, KIA, ISUZU, AUDI"
+console.log(autos.split(","))
+ 
+
+
+let letra = 'R'
+console.error(letra.charCodeAt('R'));
+
+console.log("HOLA MUNDO\t HOLA MUNDO\t HOLA MUNDO\t HOLA MUNDO");
+
+
+
+console.log('"Hola estudio "Sistema" y estoy llevando la materia de:"');
+console.log("    \t PROGRAMACION 1");
+console.log("    \tTurno noche");
+console.log("En el grupo 2")
+
+
+let random = Math.random();
+let numero = random *26
+let valorFinal = numero +50
+console.log(Math.floor(valorFinal));
+
+console.log(Libreria[8]);
+
+
+/*Convertir los siguientes valores: 🚩
+9.8     -> Numero entero
+-5      -> Numero Absoluto
+62      -> Cadena de texto
+"28"    -> Numero entero
+"15.65" -> Numero decimal*/
+
+
+let num = 9.8;
+let numInt = parseInt(num);
+
+console.log(numInt);
+
+
+
